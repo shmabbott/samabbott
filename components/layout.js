b820ca9a027a2eaf0d6519ff035e337c24e80dd9
Sam Abbott <samgeoabbott@gmail.com>
2022-07-22 11:18:05 -0700

    created initial navbar component.

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,14 @@
 import Alert from '../components/alert'
 import Footer from '../components/footer'
 import Meta from '../components/meta'
+import NavBar from './navbar'
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
+      <NavBar/>
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Footer />
---
+++ components/navbar.js
@@ -0,0 +1,7 @@
+export default function NavBar(){
+    return(
+        <div>
+            <h2>Samuel Abbott</h2>
+        </div>
+    )
+}
